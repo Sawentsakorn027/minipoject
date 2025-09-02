6bff5561cb86768df08ba52f1551001456e71935
Sawentsakorn027 <68219010027@htc.ac.th>
2025-09-02 09:43:01 +0700

    Loop027

---
+++ pt02707011_Loop.py
@@ -0,0 +1,11 @@
+sitle = 'marcuscode'
+for n in sitle:
+    print(n)
+
+names =['Mato', 'Jhon', 'Eric','Mark','Robert']
+for n in  names:
+    print(n)
+
+numbers = [10,20,30,40,50,60,70,80]
+for n in numbers:
+    print(n)
